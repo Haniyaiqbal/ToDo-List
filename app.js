f722facda6a909696b6bb1361cb9405c69158eeb
haniyaiqbal <haniyai891@gmail.com>
2023-07-23 16:05:04 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,145 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb+srv://haniyaiqbal:Hi0708@cluster0.bfqe14f.mongodb.net/todolistDB', { useNewUrlParser: true });
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todoList!"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item"
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+  name : String,
+  items : [itemsSchema]
+}
+
+const List = mongoose.model("List", listSchema); 
+
+
+
+
+app.get("/", async function(req, res) {
+  try {
+    const foundItems = await Item.find();
+
+    if (foundItems.length === 0) {
+      await Item.insertMany(defaultItems);
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+app.get("/:customNameList", async function(req, res) {
+  try {
+    const customNameList = _.capitalize(req.params.customNameList);
+    const foundList = await List.findOne({ name: customNameList });
+
+    if (!foundList) {
+      const list = new List({
+        name: customNameList,
+        items: defaultItems
+      });
+      const savedList = await list.save();
+      res.render("list", { listTitle: savedList.name, newListItems: savedList.items });
+    } else {
+      res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+
+
+app.post("/", async function(req, res) {
+  try {
+    const itemName = req.body.newItem;
+    const listName = req.body.list;
+
+    const item = new Item({
+      name: itemName
+    });
+
+    if (listName === "Today") {
+      await item.save();
+      res.redirect("/");
+    } else {
+      const foundList = await List.findOne({ name: listName });
+      foundList.items.push(item);
+      await foundList.save();
+      res.redirect("/" + listName);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+app.post("/delete", async function(req, res) {
+  try {
+    const checkedItemId = req.body.checkbox;
+    const listName = req.body.listName;
+
+    if (listName === "Today") {
+      await Item.findByIdAndRemove(checkedItemId);
+      console.log("Item deleted successfully.");
+      res.redirect("/");
+    } else {
+      await List.findOneAndUpdate(
+        { name: listName },
+        { $pull: { items: { _id: checkedItemId } } }
+      );
+      res.redirect("/" + listName);
+    }
+  } catch (err) {
+    console.log(err);
+    res.redirect("/" + listName); 
+  }
+});
+
+
+
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+app.listen(4000, function() {
+  console.log("Server started on port 4000");
+});
+ 
+
+
+
+
+//"mongodb+srv://cluster0.bfqe14f.mongodb.net/" --apiVersion 1 --username haniyaiqbal
